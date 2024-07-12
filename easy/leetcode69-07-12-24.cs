b41e400cbe550218bd2a91edd5f7477beb568685
callhimsaalim <callhimsaalim@outlook.com>
2024-07-12 12:10:54 -0500

    day 11

---
+++ easy/leetcode69-07-12-24.cs
@@ -0,0 +1,17 @@
+
+//Leetcode 69 Sqrt(x)
+
+public class Solution {
+    public int MySqrt(int x) {
+        int l = 0, r = x;
+        while (l < r) {
+            int mid = (l + r + 1) >>> 1;
+            if (mid > x / mid) {
+                r = mid - 1;
+            } else {
+                l = mid;
+            }
+        }
+        return l;
+    }
+}
